c9284937cd18e5ff65c72afafd35a7663b6a74c4
jpgm1848 <154918928+jpgm1848@users.noreply.github.com>
2024-03-11 11:37:43 -0400

    database setup done

--- server.js
+++ server.js
@@ -1,1 +1,46 @@
-console.log("hello world");
+const pg = require("pg");
+const app = express
+
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_notes_categories_db"
+);
+
+const init = async () => {
+  console.log("connecting to database");
+  await client.connect();
+  console.log("connected to database");
+  let SQL = `
+  DROP TABLE IF EXISTS notes;
+  DROP TABLE IF EXISTS categories;
+  CREATE TABLE categories(
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(100) NOT NULL
+  );
+  CREATE TABLE notes(
+    id SERIAL PRIMARY KEY, 
+    txt VARCHAR(100) NOT NULL,
+    ranking INTEGER NOT NULL DEFAULT 5,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
+    category_id INTEGER REFERENCES categories(id) NOT NULL
+  );
+  `;
+  await client.query(SQL);
+  console.log("tables created");
+
+  SQL = `
+  INSERT INTO categories(name) VALUES('foo category');
+  INSERT INTO categories(name) VALUES('bar category');
+  INSERT INTO categories(name) VALUES('bazz category');
+  INSERT INTO notes(txt, category_id) VALUES ('foo note 1', (SELECT id FROM categories WHERE name='foo category'));
+  INSERT INTO notes(txt, category_id) VALUES ('foo note 2', (SELECT id FROM categories WHERE name='foo category'));
+  INSERT INTO notes(txt, category_id) VALUES ('bar note 1', (SELECT id FROM categories WHERE name='bar category'));
+
+
+
+  `;
+  await client.query(SQL);
+  console.log("data seeded");
+};
+
+init();
